81987acc9401bca1d606be95a8e0ab2dcd781fbd
Ethan Peterson <17emp5@queensu.ca>
2019-02-02 01:42:06 -0500

    code cleanup

--- app.js
+++ app.js
@@ -1,45 +1,10 @@
-'use strict'
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-const { convert } = require('convert-svg-to-png');
+app.post('/compute', function (req, res) {
 
-const fs = require('fs')
-const path = require('path')
-const runParallel = require('run-parallel')
-const runWaterfall = require('run-waterfall')
-const pcbStackup = require('pcb-stackup')
-
-const fileNames = [
-    'arduino-uno.cmp',
-    'arduino-uno.drd',
-    'arduino-uno.gko',
-    'arduino-uno.plc',
-    'arduino-uno.sol',
-    'arduino-uno.stc',
-    'arduino-uno.sts',
-  ]
-  
-  const layers = fileNames.map(filename => ({
-    filename,
-    gerber: fs.createReadStream(filename),
-  }))
-  
-  var svg = 0
-
-  pcbStackup(layers, (error, stackup) => {
-    if (error) return console.error(error)
-  
-    console.log(stackup.top.svg) // logs "<svg ... </svg>"
-    console.log(stackup.bottom.svg) // logs "<svg ... </svg>"
-    //svg = stackup.top.svg
-  })
-
-  app.get('/', (req, res) => res.send("Hello World"))
-  app.post('/test',function(req, res) {
-      console.log(req)
-      res.send("req received")
-  });
-  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+});
+app.get('/', (req, res) => res.send("Welcome to CircuitSafe"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
--- svg-gen.py
+++ svg-gen.py
@@ -1,13 +1,16 @@
+import os
 import gerber
 from gerber.render import GerberCairoContext
 
-# Read gerber and Excellon files
-top_copper = gerber.read('shem.GTL')
-nc_drill = gerber.read('shem.txt')
+copper = gerber.read('shem.GTL')
 
 # Rendering context
 ctx = GerberCairoContext()
 
-# Create SVG image
-top_copper.render(ctx)
-nc_drill.render(ctx, 'composite.svg')
+# Create PNG
+copper.render(ctx)
+ctx.dump(os.path.join(os.path.dirname(__file__), 'output.png'))
+print("Saved")
+exit()
+
+
