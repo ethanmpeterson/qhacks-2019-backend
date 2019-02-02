41b63a7dd35e64b2e34966facb3787542f8a242f
Ethan Peterson <17emp5@queensu.ca>
2019-02-02 00:43:09 -0500

    added pcb lib fixed read issue due to incorrect filename

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
+'use strict'
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const { convert } = require('convert-svg-to-png');
 
+const fs = require('fs')
+const path = require('path')
+const runParallel = require('run-parallel')
+const runWaterfall = require('run-waterfall')
+const pcbStackup = require('pcb-stackup')
 
+const fileNames = [
+    'arduino-uno.cmp',
+    'arduino-uno.drd',
+    'arduino-uno.gko',
+    'arduino-uno.plc',
+    'arduino-uno.sol',
+    'arduino-uno.stc',
+    'arduino-uno.sts',
+  ]
+  
+  const layers = fileNames.map(filename => ({
+    filename,
+    gerber: fs.createReadStream(filename),
+  }))
+  
+  var svg = 0
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+  pcbStackup(layers, (error, stackup) => {
+    if (error) return console.error(error)
+  
+    console.log(stackup.top.svg) // logs "<svg ... </svg>"
+    console.log(stackup.bottom.svg) // logs "<svg ... </svg>"
+    //svg = stackup.top.svg
+  })
+
+  app.get('/', (req, res) => res.send("Hello World"))
+  app.post('/test',function(req, res) {
+      console.log(req)
+      res.send("req received")
+  });
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
