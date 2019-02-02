75f7eef7c076f06f05514f58a1225750699f8d87
Ethan Peterson <17emp5@queensu.ca>
2019-02-02 10:01:03 -0500

    adjusted compute script

--- compute.py
+++ compute.py
@@ -8,10 +8,12 @@ voltage = float(sys.argv[3])
 current = float(sys.argv[4])
 
 k = 0.048
-dt = 0.25
+dt = 5 # Temp raise tolerance in C
+
+traceDepth = 2.0/3.0 # in mil which is thousandth of an inch
 
 output = {}
-output['currentRating'] = k * pow(dt, 0.44) * pow(current, 0.725)
+output['currentRating'] = k * pow(dt, 0.44) * pow(traceDepth * minWidth, 0.725) # gives current rating for the PCB traces in amps
 print(json.dumps(output))
 sys.stdout.flush()
 exit()
