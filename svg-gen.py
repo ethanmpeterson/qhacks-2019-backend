1d9f7cc3b628397acffc22975c861af695ba43ad
Ethan Peterson <17emp5@queensu.ca>
2019-02-02 21:53:10 -0500

    saving progress

--- svg-gen.py
+++ svg-gen.py
@@ -1,7 +1,10 @@
 import os
 import gerber
 import sys
+import json
 from gerber.render import GerberCairoContext
+from gerber.utils import parse_gerber_value
+from gerber.rs274x import GerberParser
 
 
 copper = gerber.read('input.GTL') # argument string is the filename
@@ -11,6 +14,15 @@ ctx = GerberCairoContext()
 
 # Create PNG
 copper.render(ctx)
+# js = GerberParser.parse('input.GTL')
+# print(js)
+# # with open('data.json', 'w') as outfile:
+# #     json.dump(js, outfile)
+
+# # with open('data.json') as f:
+# #     data = json.load(f)
+# # print(data[0])
+
 ctx.dump(os.path.join(os.path.dirname(__file__), 'output.png'))
 #print("Saved")
 exit()
